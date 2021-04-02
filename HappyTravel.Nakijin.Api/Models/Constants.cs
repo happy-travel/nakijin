6d52af50ae6206fafe323031f09ad1193e5ce68e
Hasmik <hasmik_harutyunyan@outlook.com>
2021-04-02 18:23:39 +0400

    changed preloading batch size

--- HappyTravel.Nakijin.Api/Models/Constants.cs
+++ HappyTravel.Nakijin.Api/Models/Constants.cs
@@ -3,7 +3,7 @@ namespace HappyTravel.Nakijin.Api.Models
     public static class Constants
     {
         public const string DefaultLanguageCode = "en";
-        public const int DefaultPreloadingBatchSize = 6000;
+        public const int DefaultPreloadingBatchSize = 3000;
         public const int DefaultMappingBatchSize = 4000;
         public const int DefaultMergingBatchSize = 2000;
         public const int DefaultDbCommandTimeOut = 300;
