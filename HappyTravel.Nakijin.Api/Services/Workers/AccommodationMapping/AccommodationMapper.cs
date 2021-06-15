8db5f1d74a4dffae391d9bc299598fec8539edd1
Hasmik <hasmik_harutyunyan@outlook.com>
2021-06-15 16:36:21 +0400

    fixes bug, change normalization priority

--- HappyTravel.Nakijin.Api/Services/Workers/AccommodationMapping/AccommodationMapper.cs
+++ HappyTravel.Nakijin.Api/Services/Workers/AccommodationMapping/AccommodationMapper.cs
@@ -211,13 +211,13 @@ namespace HappyTravel.Nakijin.Api.Services.Workers.AccommodationMapping
 
             foreach (var accommodation in accommodationsToMap)
             {
+                var normalized = _multilingualDataHelper.NormalizeAccommodation(accommodation);
                 if (!accommodation.IsActive)
                 {
                     IgnoreOrDeactivateDeactivatedOnSupplier(accommodation);
                     continue;
                 }
 
-                var normalized = _multilingualDataHelper.NormalizeAccommodation(accommodation);
                 if (normalized.Location.Coordinates.IsEmpty() || !normalized.Location.Coordinates.IsValid())
                 {
                     _logger.LogNotValidCoordinatesInAccommodation(
